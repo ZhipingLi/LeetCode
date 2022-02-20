c9cc67cb568b6088129e306c35a55433708e65e4
Zhiping <zhiping.li@wsu.edu>
2022-02-20 02:10:45 -0800

    Parenthesis topic

---
+++ examples/括号题目/1541. Minimum Insertions to Balance a Parentheses String/mySolution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var minInsertions = function(s) {
+    let res = 0, need = 0 // 操作res值表示结算
+    for(c of s.split('')){
+        if(c === '('){
+            need+=2
+            if(need % 2 === 1){
+                // 插⼊⼀个右括号 
+                res += 1 // 结算之前在平衡状态下插入'}'时用于匹配的'}'
+                // 对右括号的需求减⼀
+                need--
+            }
+        }else{
+            need--
+            if(need === -1){
+                res++ // 结算在平衡状态下插入'}'时用于匹配的'{'
+                need = 1
+            }
+        }
+    }
+    return res+need
+};
